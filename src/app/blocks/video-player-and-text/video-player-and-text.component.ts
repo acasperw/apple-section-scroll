75a4a126e43bcf2e8bd94bde9c615f671c583c5d
Alex Whitlock <zanderwhitlock@gmail.com>
2023-09-29 16:20:45 +0100

    Update

--- src/app/blocks/video-player-and-text/video-player-and-text.component.ts
+++ src/app/blocks/video-player-and-text/video-player-and-text.component.ts
@@ -17,11 +17,15 @@ export class VideoPlayerAndTextComponent implements AfterViewInit {
 
 
       const element = this.el.nativeElement;
+      const contentElement = element.querySelector('.container__patter_block_inner');
       const percentage = this.getElementScrollThroughPercentage(element);
+      
+      const contentHeight: number = contentElement.clientHeight;
 
       if (percentage !== null) {
 
-        this.transformScrollVal = `matrix(1, 0, 0, 1, 0, ${percentage})`;
+        // this.transformScrollVal = `matrix(1, 0, 0, 1, 0, ${percentage})`;
+        this.transformScrollVal = `matrix(1, 0, 0, 1, 0, ${percentage + contentHeight*2})`;
 
         console.log('percentage', this.transformScrollVal);
 
@@ -41,6 +45,7 @@ export class VideoPlayerAndTextComponent implements AfterViewInit {
     const elementHeight = element.clientHeight;
 
     const scrolledPast = windowHeight - rect.top;
+
     return elementHeight - (2 * scrolledPast);
   }
 
