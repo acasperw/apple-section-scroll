febe4fec7296164dd2100767763cf63d6f93d53b
Alexander Whitlock <zanderwhitlock@gmail.com>
2023-09-29 10:24:21 +0100

    mini style updates

--- src/app/helpers/block-full-width.directive.ts
+++ src/app/helpers/block-full-width.directive.ts
@@ -5,7 +5,7 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
 })
 export class BlockFullWidthDirective {
   constructor(private renderer: Renderer2, hostElement: ElementRef) {
-    renderer.addClass(hostElement.nativeElement, 'wp-block');
+    renderer.addClass(hostElement.nativeElement, 'wp-block-patter-block-wrapper-000');
     renderer.addClass(hostElement.nativeElement, 'alignfull');
   }
 }
